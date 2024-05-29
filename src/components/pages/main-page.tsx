0382aff9b0a4385775dc98e654f21ca5d4bd107a
SajdaHouse <sajdahouse1@gmail.com>
2024-05-29 22:14:36 +0300

    Update main-page.tsx
    add google analytics to main page

--- src/components/pages/main-page.tsx
+++ src/components/pages/main-page.tsx
@@ -10,7 +10,20 @@ import { useCallback, useEffect, useState } from "react";
 import { productType } from "@/lib/types";
 import { getProductsArray } from "@/lib/actions";
 import { arraymove } from "@/lib/utils";
-
+import Script from "next/script";
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+<Script
+  src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+  strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${G-2FPWD9QP9G}');
+  `}
+</Script>
 export default function MainPage() {
   const categories = [
     {
